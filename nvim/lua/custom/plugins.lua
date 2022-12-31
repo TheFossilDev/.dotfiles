4e77175f5c423e74c942c58a40a459616f04d029
Cole <colewestbrookphoto@gmail.com>
2022-12-31 12:55:32 -0500

    Basic kickstarter setup
    Modular setup which is upgrade-ready

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,9 @@
+require "custom.autopairs"
+return function(use)
+	use {
+		"windwp/nvim-autopairs",
+		config = function() require("nvim-autopairs").setup {} end
+	}
+	use 'preservim/nerdtree'
+	use 'ryanoasis/vim-devicons'
+end
--- nvim/lua/fossildev/remap.lua
+++
@@ -1,6 +0,0 @@
-print("Hello from remaps")
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>q", ":q<CR>")
-vim.keymap.set("n", "<leader>aq", ":qa<CR>")
-vim.keymap.set("n", "<leader>wq", ":wq<CR>")
-vim.keymap.set("n", "<leader>waq", ":wqa<CR>")
