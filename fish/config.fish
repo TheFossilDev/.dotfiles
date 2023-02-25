0217a6043bb54aac1f24bcd40009cf7159ef96eb
Cole <colewestbrookphoto@gmail.com>
2023-02-25 17:57:00 -0500

    Merge non-vim configs from 'nvchad'

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+starship init fish | source
