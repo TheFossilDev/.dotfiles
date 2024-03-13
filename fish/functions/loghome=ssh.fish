e142314d79c447f542d44602ac9b4c82ba43d586
Cole <52761726+TheFossilDev@users.noreply.github.com>
2024-03-13 18:02:09 -0400

    Idek

---
+++ fish/functions/loghome=ssh.fish
@@ -0,0 +1,4 @@
+function loghome=ssh --wraps=colewestbrook@192.168.0.108 --description 'alias loghome=ssh colewestbrook@192.168.0.108'
+  colewestbrook@192.168.0.108 $argv
+        
+end
---
+++ fish/functions/nest.fish
@@ -0,0 +1,4 @@
+function nest --wraps='ssh colewestbrook@192.168.0.181' --description 'alias nest ssh colewestbrook@192.168.0.181'
+  ssh colewestbrook@192.168.0.181 $argv
+        
+end
